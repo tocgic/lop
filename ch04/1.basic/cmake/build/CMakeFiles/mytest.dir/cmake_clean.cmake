0926a228cc4714e716c1403698c171c76a03f733
jeongyon.cho <jeongyon.cho@gmail.com>
2019-08-02 11:32:40 +0900

    cmake -H. -Bbuild 로 생성

---
+++ ch04/1.basic/cmake/build/CMakeFiles/mytest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mytest.dir/test1.c.o"
+  "CMakeFiles/mytest.dir/test2.c.o"
+  "CMakeFiles/mytest.dir/test3.c.o"
+  "mytest.pdb"
+  "mytest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mytest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
